5fecffa7d015e953343ce43f88984cc29961332e
lucastribioliresale <lucas.tribioli@resale.com.br>
2022-12-12 09:49:59 -0300

    adjustments in docker-compose

--- celery/celery.py
+++ celery/celery.py
@@ -3,3 +3,5 @@ from celery import Celery
 
 # Primeiro argumento é o nome do módulo, segundo é o nome da fila, terceiro é o result backend (é onde é armazenado o que a tarefa vai retornar)
 app = Celery('celery',broker='amqp://admin:mypass@10.211.55.12:5672',backend='rpc://',include=['celery.tasks'])
+
+__all__ = ('celery',)
